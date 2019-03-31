656e88d00d69a71a6bfe169bae6ae6b7ee05282b
aditi <aditi@empower-ed.in>
2019-03-31 14:28:45 +0530

    Add hello component

---
+++ src/components/hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './hello.css';
+
+class Hello extends React.Component {
+    render () {
+
+        return (
+            <h1> Hello Aditi...!!!</h1>
+        ); 
+    }
+}
+
+export default Hello;
+
